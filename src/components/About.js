72403f05d51f9e2d57f03a12fcc542fe21b7864b
Sataphon Obra <puye53991@gmail.com>
2023-06-20 09:41:18 +0700

    add message

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About() {
         <section id="about">
             <div>
                 <h1>hi</h1>
+                <h1>hi2</h1>
             </div>
         </section>
     );
